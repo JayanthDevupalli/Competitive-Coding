cec161c008146d1c02790af29b156601833ffaa1
Jayanth <devupallijayanth@gmail.com>
2025-02-14 22:10:09 +0530

    codechef

---
+++ CodeChef/CodeChefRound.c
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/CODECHEF?tab=statement
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+    int n;
+    scanf("%d",&n);
+    if(n==4)
+    {
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
+
