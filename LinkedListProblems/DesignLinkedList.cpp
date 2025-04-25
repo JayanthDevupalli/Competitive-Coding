5a13c87378ae4cdf7f0910e7f8d1db8f3ac1519f
Jayanth <devupallijayanth@gmail.com>
2025-04-25 12:18:03 +0530

    LinkedList

---
+++ LinkedListProblems/DesignLinkedList.cpp
@@ -0,0 +1,110 @@
+//Link: https://leetcode.com/problems/design-linked-list/
+
+// Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
+// A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
+// If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+
+
+class Node {
+    public:
+        int val;
+        Node* next;
+        Node(int val) {
+            this->val = val;
+            this->next = nullptr;
+        }
+    };
+    
+    class MyLinkedList {
+    public:
+        Node* head;
+        int size;
+        MyLinkedList() {
+            head = nullptr;
+            size = 0;
+        }
+    
+        int get(int index) {
+            if (index >= size || index < 0) return -1;
+            Node* temp = head;
+            for (int i = 0; i < index; i++){
+                temp = temp->next;
+            }
+            return temp->val;
+        }
+    
+        void addAtHead(int val) {
+            Node* nNode = new Node(val);
+            nNode->next = head;
+            head = nNode;
+            size++;
+        }
+    
+        void addAtTail(int val) {
+            Node* nNode = new Node(val);
+            if (!head) {
+                head = nNode;
+            } else {
+                Node* temp = head;
+                while (temp->next) temp = temp->next;
+                temp->next = nNode;
+            }
+            size++;
+        }
+    
+        void addAtIndex(int index, int val) {
+            if (index < 0 || index > size) return;
+            if (index == 0) {
+                addAtHead(val);
+                return;
+            }
+            if (index == size) {
+                addAtTail(val);
+                return;
+            }
+            Node* nNode = new Node(val);
+            Node* temp = head;
+            for (int i = 0; i < index - 1; i++){
+                 temp = temp->next;
+            }
+            nNode->next = temp->next;
+            temp->next = nNode;
+            size++;
+        }
+    
+        void deleteAtHead() {
+            if (!head) return;
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            size--;
+        }
+    
+        void deleteAtTail() {
+            if (!head) return;
+            if (!head->next) {
+                delete head;
+                head = nullptr;
+            } else {
+                Node* curr = head;
+                while (curr->next->next) curr = curr->next;
+                delete curr->next;
+                curr->next = nullptr;
+            }
+            size--;
+        }
+    
+        void deleteAtIndex(int index) {
+            if (index < 0 || index >= size) return;
+            if (index == 0) {
+                deleteAtHead();
+            } else {
+                Node* curr = head;
+                for (int i = 0; i < index - 1; i++) curr = curr->next;
+                Node* toDelete = curr->next;
+                curr->next = curr->next->next;
+                delete toDelete;
+                size--;
+            }
+        }
+};
